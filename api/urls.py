23fc615b3a6ce912af6dfb00fd9e3650c66fbdf8
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-08 11:57:36 +0500

    create api django app

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+
+
+urlpatterns = [
+    path("v1/", include("api.v1.urls")),
+
+]
---
+++ api/v1/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+urlpatterns = [
+    path("", include("goods.urls")),
+    path("", include("users.urls")),
+    path("", include("orders.urls")),
+
+]
